b12449fdd30e5f13dc9ecf42d8c880be2b62ffc0
itapi <itapiatu12@gmail.com>
2016-09-16 12:46:34 +0300

    hello
    hello

---
+++ ScreenShareClient/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace ScreenShareClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Enabled = false;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(295, 317);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(91, 180);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(31, 100);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "127.0.0.1";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(168, 100);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "1998";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(295, 317);
+            this.panel1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(295, 317);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        public System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ ScreenShareClient/Form1.cs
@@ -0,0 +1,344 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using SharpDX;
+using System.Threading.Tasks;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Net;
+using System.Diagnostics;
+using System.Threading;
+using System.Net.Sockets;
+using System.Windows.Forms;
+
+namespace ScreenShareClient
+{
+    public partial class Form1 : Form
+    {
+
+        private TcpClient client;
+        private Socket sck;
+        private Bitmap initial,block;
+        private byte[] buffer;
+        private Thread thread;
+        private MemoryStream ms;
+        private double mbs = 0;
+        private int x, y;
+        private Rectangle BlockRectangle=new Rectangle(0,0,0,0);
+        public Form1()
+        {
+            buffer = new byte[1920 * 1080 * 3];
+            ms = new MemoryStream(buffer, 4, buffer.Length - 4);
+            InitializeComponent();
+        }
+        private void PrintData()
+        {
+
+            Console.WriteLine("DataLength :" + length);
+            Console.WriteLine("Block X : {0}  Y : {1}", x, y);
+            Console.WriteLine("--------");
+        }
+
+        private Bitmap bufferToJpeg()
+        {
+
+
+            return (Bitmap)Image.FromStream(ms);
+        }
+
+        private int BlockX()
+        {
+            return buffer[0] | buffer[1] << 8;
+
+        }
+
+        private int BlockY()
+        {
+            return buffer[2] | buffer[3] << 8;
+
+        }
+        int length;
+
+        private int ReadData()
+        {
+            int total = 0;
+            int recv;
+            byte[] datasize = new byte[4];
+
+            recv = sck.Receive(datasize, 0, 4, 0);
+            int size = BitConverter.ToInt32(datasize, 0);
+            int dataleft = size;
+
+            while (total < size)
+            {
+                recv = sck.Receive(buffer, total, dataleft, 0);
+                if (recv == 0)
+                {
+
+                    break;
+                }
+                total += recv;
+                dataleft -= recv;
+            }
+           //// if(size>65000)
+           // Console.WriteLine(size / 1000 + "KB");
+            return size;
+        }
+        private int ReadData2()
+        {
+
+            int sum = 0;
+            byte[] header = new byte[4];
+            sck.Receive(header);
+            length = BitConverter.ToInt32(header, 0);
+            sum = sck.Receive(buffer, length, SocketFlags.None);
+            while (sum < length)
+            {
+                sum += sck.Receive(buffer, length, SocketFlags.None);
+                Console.WriteLine("reading....");
+
+            }
+           
+            return length;
+
+
+        }
+        private unsafe void Draw2(Bitmap bmp2, Point point)
+        {
+            lock (initial)
+            {
+                BitmapData bmData = initial.LockBits(new Rectangle(0, 0, initial.Width, initial.Height), System.Drawing.Imaging.ImageLockMode.WriteOnly, initial.PixelFormat);
+                BitmapData bmData2 = bmp2.LockBits(new Rectangle(0, 0, bmp2.Width, bmp2.Height), System.Drawing.Imaging.ImageLockMode.ReadOnly, bmp2.PixelFormat);
+                IntPtr scan0 = bmData.Scan0;
+                IntPtr scan02 = bmData2.Scan0;
+                int stride = bmData.Stride;
+                int stride2 = bmData2.Stride;
+                int Width = bmp2.Width;
+                int Height = bmp2.Height;
+                int X = point.X;
+                int Y = point.Y;
+
+                scan0 = IntPtr.Add(scan0, stride * Y + X * 3);//setting the pointer to the requested line
+                for (int y = 0; y < Height; y++)
+                {
+                    memcpy(scan0, scan02, (UIntPtr)(Width * 3));//copy one line
+
+                    scan02 = IntPtr.Add(scan02, stride2);//advance pointers
+                    scan0 = IntPtr.Add(scan0, stride);//advance pointers//
+                }
+
+
+                initial.UnlockBits(bmData);
+                bmp2.UnlockBits(bmData2);
+            }
+        }
+        [DllImport("msvcrt.dll", EntryPoint = "memcpy", CallingConvention = CallingConvention.Cdecl, SetLastError = false)]
+        public static extern IntPtr memcpy(IntPtr dest, IntPtr src, UIntPtr count);
+        private unsafe void Draw(Bitmap bmp2, Point point)
+        {
+            lock(initial)
+            { 
+          
+                    Rectangle LockRectangle = new Rectangle(point.X, point.Y, bmp2.Width, bmp2.Height);
+                   // Console.WriteLine(initial.Size.ToString());
+                    
+                     //Console.WriteLine(LockRectangle.ToString());
+                         BitmapData bmData = initial.LockBits(LockRectangle, System.Drawing.Imaging.ImageLockMode.WriteOnly, initial.PixelFormat);
+                    BitmapData bmData2 = bmp2.LockBits(new Rectangle(0, 0, bmp2.Width, bmp2.Height), System.Drawing.Imaging.ImageLockMode.ReadOnly, bmp2.PixelFormat);
+                    IntPtr scan0 = bmData.Scan0;
+                    IntPtr scan02 = bmData2.Scan0;
+                    int stride = bmData.Stride;
+                    int stride2 = bmData2.Stride;
+                    int Width = bmp2.Width;
+                    int Height = bmp2.Height;
+                    int X = point.X;
+                    int Y = point.Y;
+
+                    // scan0 = IntPtr.Add(scan0, stride * Y + X * 3);//setting the pointer to the requested line
+                    for (int y = 0; y < Height; y++)
+                    {
+                        Utilities.CopyMemory(scan0, scan02, (Width * 3));//copy one line
+
+                        scan02 = IntPtr.Add(scan02, stride2);//advance pointers
+                        scan0 = IntPtr.Add(scan0, stride);//advance pointers//
+                    }
+
+                    /* #region checkInvalideUpdate
+                      for (int y=0;y<initial.Height;y++)
+                      {
+                          scan0 = bmData.Scan0;
+                          byte* ptr = (byte*)scan0;
+                          ptr += y * stride;
+                          for (int x = 0; x < initial.Width; x++)
+                          {
+                              ptr[0] = 255;
+                              ptr[1] = 0;
+                              ptr[2] = 0;
+                              ptr += 3;
+                          }
+                      }
+                      #endregion*/
+                    initial.UnlockBits(bmData);
+                    bmp2.UnlockBits(bmData2);
+
+              
+            }
+        }
+
+        private Rectangle GetViewRect() { return pictureBox1.ClientRectangle; }
+
+        private void MainScreenThread()
+        {
+
+            pictureBox1.Resize += (_s, _e) =>
+            {
+                viewRect = GetViewRect();
+                scaleX = (float)viewRect.Width / initial.Width;
+                scaleY = (float)viewRect.Height / initial.Height;
+                scaleXRev = (float)initial.Width / viewRect.Width;
+                scaleYRev = (float)initial.Height / viewRect.Height;
+                pictureBox1.Invalidate();
+            };
+
+            // The update action
+            Action updateAction = () =>
+            {
+
+                var targetRect = Rectangle.FromLTRB(
+                    (int)Math.Truncate(BlockRectangle.X * scaleX),
+                    (int)Math.Truncate(BlockRectangle.Y * scaleY),
+                    (int)Math.Ceiling(BlockRectangle.Right * scaleX),
+                    (int)Math.Ceiling(BlockRectangle.Bottom * scaleY));
+                // Console.WriteLine(BlockRectangle);
+                pictureBox1.Invalidate(targetRect);
+                
+            };
+
+            ReadData();//reading data from socket.
+            client.Close();
+            initial = bufferToJpeg();//first intial full screen image.
+           
+            pictureBox1.Paint += pictureBox1_Paint;
+            BlockRectangle.X = 0;
+            BlockRectangle.Y = 0;
+            BlockRectangle.Width = initial.Width;
+            BlockRectangle.Height = initial.Height;
+            viewRect = GetViewRect();
+            scaleX = (float)viewRect.Width / initial.Width;
+            scaleY = (float)viewRect.Height / initial.Height;
+            scaleXRev = (float)initial.Width / viewRect.Width;
+            scaleYRev = (float)initial.Height / viewRect.Height;
+            this.Invoke(updateAction);
+            while (true)
+            {
+
+                var receivedData = udpclient.Receive(ref ep);
+                //Console.WriteLine(receivedData.Length);
+
+                x = receivedData[0] | receivedData[1] << 8;
+                y = receivedData[2] | receivedData[3] << 8;
+             //   Console.WriteLine(x + ", " + y);
+                    block = Tojpeg(receivedData);//constantly reciving blocks.
+                    Draw(block, new Point(x, y));//applying the changes-drawing the block on the big initial image.using native memcpy.
+            
+               
+                    // Invoke the update action, passing the updated block rectangle
+                    BlockRectangle.X = x;
+                    BlockRectangle.Y = y;
+                    BlockRectangle.Width = block.Width;
+                    BlockRectangle.Height = block.Height;
+                    this.Invoke(updateAction);
+               
+            }
+        }
+
+     
+        private Bitmap Tojpeg(byte[]buffer)
+        {
+            using (MemoryStream ms =new MemoryStream(buffer,4,buffer.Length-4))
+            {
+                
+                return (Bitmap)Image.FromStream(ms);
+            }
+        }
+        Rectangle viewRect;
+        float scaleXRev,scaleYRev,scaleX, scaleY;
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+
+            lock (initial)
+            {         
+                var targetRect = e.ClipRectangle;
+
+                //Console.WriteLine(scaleX + " , " + scaleY);
+                //MessageBox.Show(e.Graphics.SmoothingMode.ToString());
+
+                var imageRect = new RectangleF(targetRect.X * scaleXRev, targetRect.Y * scaleYRev, targetRect.Width * scaleXRev, targetRect.Height * scaleYRev);
+                //e.Graphics.FillRectangle(Brushes.Red,new Rectangle(targetRect.X-1,targetRect.Y-1,targetRect.Width-1,targetRect.Height-1));
+                //Console.WriteLine(targetRect.ToString()+" ===>"+imageRect.ToString());
+                //Console.WriteLine(targetRect);
+                e.Graphics.DrawImage(initial, targetRect, imageRect, GraphicsUnit.Pixel);
+            }
+            
+        }
+        int count = 0;
+
+        UdpClient udpclient;
+        IPEndPoint ep;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+             client = new TcpClient(textBox1.Text, 25655);
+             sck = client.Client;          
+             udpclient = new UdpClient();
+             ep = new IPEndPoint(IPAddress.Parse(textBox1.Text), 1100); // endpoint where server is listening
+             udpclient.Connect(ep);
+             Thread.Sleep(500);
+             udpclient.Send(new byte[] { 1 }, 1);
+             panel1.Visible = false;
+             pictureBox1.Visible = true;
+             thread = new Thread(MainScreenThread);
+             thread.Start();
+        }
+
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            //  this.Text = m.ToString() +"MBP/S";
+
+            //mbs = 0;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+
+    }
+}
+        /// </summary>
+        public static class NativeMethods
+        {
+
+            [DllImport("msvcrt.dll", CallingConvention = CallingConvention.Cdecl)]
+            public static extern unsafe int memcmp(byte* ptr1, byte* ptr2, uint count);
+
+            [DllImport("msvcrt.dll", CallingConvention = CallingConvention.Cdecl)]
+            public static extern int memcmp(IntPtr ptr1, IntPtr ptr2, uint count);
+
+            [DllImport("msvcrt.dll", CallingConvention = CallingConvention.Cdecl)]
+            public static extern int memcpy(IntPtr dst, IntPtr src, uint count);
+
+            [DllImport("msvcrt.dll", CallingConvention = CallingConvention.Cdecl)]
+            public static extern unsafe int memcpy(void* dst, void* src, uint count);
+        }
+    
+
